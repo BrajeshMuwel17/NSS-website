119344f5608fc1a56262409a181ff9e3fc332cfd
BrajeshMuwel17 <157781838+BrajeshMuwel17@users.noreply.github.com>
2024-04-18 22:42:11 +0530

    Edited Profile.js

--- newnss/src/components/Profile.js
+++ newnss/src/components/Profile.js
@@ -2,28 +2,44 @@ import React from 'react';
 import debangaImage from '../images/banner.jpg';
 import janeImage from '../images/banner.jpg';
 import './Profile.css';
+import { MDBCard, MDBCardBody, MDBCardImage, MDBTypography } from 'mdb-react-ui-kit';
 
 const TeamMember = (member) => {
   // console.log(member);
   const { name, designation, imagelink } = member;
   console.log(imagelink);
   return (
-    <div className="Profile-box">
-      <div className="profile-pic">
-        <img className="pic"
-          src={imagelink}
-          alt="image"
-          style={{
-            borderRadius: '50%',
-            width: '90%',
-            height: '90%',
-            objectFit: 'cover',
-          }}
-        />
-      </div>
-      <h3>{name}</h3>
-      <p>{designation}</p>
-    </div>
+    // <div className="Profile-box">
+    //   <div className="profile-pic">
+    //     <img className="pic"
+    //       src={imagelink}
+    //       alt="image"
+    //       style={{
+    //         borderRadius: '50%',
+    //         width: '90%',
+    //         height: '90%',
+    //         objectFit: 'cover',
+    //       }}
+    //     />
+    //   </div>
+    //   <h3>{name}</h3>
+    //   <p>{designation}</p>
+    // </div>
+        <div className="vh-100 d-flex align-items-center justify-content-center" style={{ backgroundColor: '#eee' }}>
+      <MDBCard className="card">
+        <MDBCardBody className="text-center">
+          <div className="profile-image">
+            <div className="mt-3 mb-4 card-image">
+              <a href="/">
+                <MDBCardImage src={imagelink} alt="Profile Image" className="rounded-circle" />
+              </a>
+            </div>
+          </div>
+          <MDBTypography tag="h4" className="card-title">{name}</MDBTypography>
+          <MDBTypography tag="h4" className="designation">{designation}</MDBTypography>
+        </MDBCardBody>
+      </MDBCard>
+     </div>
   );
 };
 
@@ -57,3 +73,30 @@ export default Profile;
         </div>
         <Profile/>
       </div> */}
+
+
+
+// import React from 'react';
+// import { MDBCard, MDBCardBody, MDBCardImage, MDBTypography } from 'mdb-react-ui-kit';
+// import './Profile.css'; // Import the CSS file
+// import ok from '../images/team-8.jpg';
+
+// export default function Profile() {
+//   return (
+//     <div className="vh-100 d-flex align-items-center justify-content-center" style={{ backgroundColor: '#eee' }}>
+//       <MDBCard className="card">
+//         <MDBCardBody className="text-center">
+//           <div className="profile-image">
+//             <div className="mt-3 mb-4 card-image">
+//               <a href="/">
+//                 <MDBCardImage src={ok} alt="Profile Image" className="rounded-circle" />
+//               </a>
+//             </div>
+//           </div>
+//           <MDBTypography tag="h4" className="card-title">Dr. Debanga Raj Neogjk</MDBTypography>
+//           <MDBTypography tag="h4" className="designation">ASSISTANT  OFFICER</MDBTypography>
+//         </MDBCardBody>
+//       </MDBCard>
+//     </div>
+//   );
+// }
